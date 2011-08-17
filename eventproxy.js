12410f4ac486e5ca713c5a2f0cc2de639d57f2a4
dead-horse <dead_horse@qq.com>
2011-08-17 17:26:17 +0800

    对assign函数的修改。  1.改为采用计数的方式，因为本来对once里面的回调函数只会执行一次，因此在执行的时候对这个times++，而在触发'all'事件的时候，检测timse是否等于所监听的事件数目，当timse===length的时候，说明所有事件都已经被触发，执行cb函数。（数据的传递还是按照之前的方式）  2.之前assign函数是只监听一次的，和once类似。我的想法是在函数结尾的时候，通过再对每一个事件调用一次bind方法，重新建立它们的绑定，可以让它像on一样工作，每次触发完所有事件后又开始重新监听这几个事件。暂时只是在函数参数列表尾的时候增加了一个参数，当函数传进来的最后一个参数是"persistent"的时候，就持续监听，否则和以前一样。

--- eventproxy.js
+++ eventproxy.js
@@ -117,15 +117,34 @@ EventProxy.prototype.once = function (ev, callback) {
  * @param {string} eventname1 First event name.
  * @param {string} eventname2 Second event name.
  * @param {function} cb Callback, that will be called after predefined events were fired.
+ * @param {string} persistent, if persistent==="persistent", this function will
  */
-EventProxy.prototype.assign = function (eventname1, eventname2, cb) {
-    var proxy = this, length, index = 0, argsLength = arguments.length;
+
+EventProxy.prototype.assign = function (eventname1, eventname2, cb, persistent) {
+    var proxy = this, length, index = 0, callback, argsLength = arguments.length,
+    lastArg = arguments[argsLength - 1],
+     times=0, isOnce, events;
     // Check the arguments length.
-    if (argsLength < 2) {
-        return this;
+    if(typeof lastArg === "function"){        
+    	if (argsLength < 2) {
+        	return this;
+    	}
+    	callback = lastArg;
+    	isOnce = true;
+    	events = [].slice.apply(arguments, [0, argsLength - 1]);
     }
-    var events = [].slice.apply(arguments, [0, argsLength - 1]);
-    var callback = arguments[argsLength - 1];
+    else if(lastArg === "persistent"){
+    	if(argsLength < 3)
+    		return this;
+    	isOnce = false;
+    	callback = arguments[argsLength - 2];
+		if(typeof callback!=="function")
+			return this;
+    	events = [].slice.apply(arguments, [0, argsLength - 2]);
+    }
+    else{
+    	return this;
+    }
     // Check the callback type.
     if (typeof callback !== "function") {
         return this;
@@ -136,34 +155,37 @@ EventProxy.prototype.assign = function (eventname1, eventname2, cb) {
             proxy.once(key, function (data) {
                     proxy._fired[key] = proxy._fired[key] || {};
                     var flag = proxy._fired[key];
-                    flag.ready = true;
-                    flag.data = data;
+                    flag.data = [].slice.call(arguments, 0);
+                    times++;
                 });
         };
     for (index = 0; index < length; index++) {
         bind(events[index]);
     }
-
     var all = function () {
-        var fire = true, data = [];
-        for (index = 0; index < length; index++) {
-            if (proxy._fired[events[index]] && proxy._fired[events[index]].ready) {
-                data.push(proxy._fired[events[index]].data);
-            } else {
-                fire = false;
-                break;
-            }
-        }
-
-        if (fire) {
-            proxy.unbind("all", all);
-            callback.apply(null, data);
-        }
+    	if(times<length)
+    		return;
+    	var data=[];
+    	for(index=0; index<length; ++index){
+    		if(proxy._fired[events[index]].data){
+    			data.push(proxy._fired[events[index]].data);
+    		}
+    	}
+       callback.apply(null, data);
+       if(!isOnce){	//检测是否只监听一次，如果不是，则重新绑定每个事件（调用bind函数），同时times清0，不解除'all'事件的绑定。
+	       for (index = 0; index < length; index++) {
+	       		bind(events[index]);
+	       }
+	       	times = 0;
+       }
+       else{//反之如果只监听一次，则解除'all'事件的绑定
+       		proxy.unbind("all", all);
+       }
     };
-
     proxy.bind("all", all);
     return this;
 };
+
 
 // Event proxy can be used in browser and Nodejs both.
 if (typeof exports !== "undefined") {
