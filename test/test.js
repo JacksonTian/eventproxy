b553892906c3986e1df9d222fa627e4f17d40c51
Jackson Tian <shyvo1987@gmail.com>
2011-09-19 02:16:46 -0700

    Add test.

--- test/test.js
+++ test/test.js
@@ -1,24 +1,99 @@
-var EventProxy = require('./eventproxy.js').EventProxy;
-
-var proxy = new EventProxy();
-
-proxy.assign("ev1", "ev2", "ev3", "ev4", function () {
-    console.log("assign once!");
-    console.log(arguments);
+test("EventProxy - bind/trigger", function() {
+    var obj = new EventProxy();
+    var counter = 0;
+    obj.bind("event", function(data) {
+        counter += 1;
+    });
+    obj.trigger("event");
+    equals(counter, 1, 'Counter should be incremented.');
+    obj.trigger("event");
+    equals(counter, 2, 'Counter should be incremented.');
+    obj.trigger("event");
+    equals(counter, 3, 'Counter should be incremented.');
+    obj.trigger("event");
+    equals(counter, 4, 'Counter should be incremented.');
 });
-proxy.assignAll("ev1", "ev2", "ev3", "ev4", function () {
-    console.log("assign all!");
-    console.log(arguments);
+
+test("EventProxy - bind, then unbind all functions", function () {
+    var obj = new EventProxy();
+    var counter = 0;
+    obj.bind('event', function() {
+        counter += 1;
+    });
+    obj.trigger('event');
+    equals(counter, 1, 'counter should be incremented.');
+    obj.unbind('event');
+    obj.trigger('event');
+    equals(counter, 1, 'counter should have only been incremented once.');
 });
 
-console.log("first");
-proxy.emit("ev1", 1);
-proxy.emit("ev2", 2);
-proxy.emit("ev3", 3);
-proxy.emit("ev4", 4);
-console.log("second");
-proxy.emit("ev4", 5);
-proxy.emit("ev4", 6);
-proxy.emit("ev4", 7);
-proxy.emit("ev4", 8);
-console.log("third");
+test("EventProxy - once/trigger", function () {
+    var obj = new EventProxy();
+    var counter = 0;
+    obj.once('event', function() {
+        counter += 1;
+    });
+    obj.trigger('event');
+    equals(counter, 1, 'counter should be incremented.');
+    obj.trigger('event');
+    equals(counter, 1, 'counter should have only been incremented once.');
+});
+
+test("EventProxy - assign one event", function () {
+    var obj = new EventProxy();
+    var counter = 0;
+    obj.assign('event', function() {
+        counter += 1;
+    });
+    obj.trigger('event');
+    equals(counter, 1, 'counter should be incremented.');
+});
+
+test("EventProxy - assign two events", function () {
+    var obj = new EventProxy();
+    var counter = 0;
+    obj.assign('event1', 'event2', function() {
+        counter += 1;
+    });
+    obj.trigger('event1');
+    equals(counter, 0, 'counter should not be incremented.');
+    obj.trigger('event2');
+    equals(counter, 1, 'counter should be incremented.');
+    obj.trigger('event2');
+    equals(counter, 1, 'counter should have only been incremented once.');
+});
+
+
+test("EventProxy - assignAlways", function () {
+    var obj = new EventProxy();
+    var counter = 0;
+    obj.assignAlways('event1', 'event2', function() {
+        counter += 1;
+    });
+    obj.trigger('event1');
+    equals(counter, 0, 'counter should not be incremented.');
+    obj.trigger('event2');
+    equals(counter, 1, 'counter should be incremented.');
+    obj.trigger('event2');
+    equals(counter, 2, 'counter should be incremented.');
+});
+
+test("EventProxy - after, 5 times", function () {
+    var obj = new EventProxy();
+    var counter = 0;
+    obj.after('event', 5, function() {
+        counter += 1;
+    });
+    obj.trigger('event');
+    equals(counter, 0, 'counter should not be incremented.');
+    obj.trigger('event');
+    equals(counter, 0, 'counter should not be incremented.');
+    obj.trigger('event');
+    equals(counter, 0, 'counter should not be incremented.');
+    obj.trigger('event');
+    equals(counter, 0, 'counter should not be incremented.');
+    obj.trigger('event');
+    equals(counter, 1, 'counter should be incremented.');
+    obj.trigger('event');
+    equals(counter, 1, 'counter should have only been incremented once.');
+});
